a6d2580722209c206dd1fadd68a0ce1b319c542a
Erik Carvalho <erik.carvalho94@hotmail.com>
2024-03-06 22:25:41 -0300

    refiz o codigo

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 let entrada = '';
-while (entrada.toLowerCase() !== 'sair') {
+while (true) {
     entrada = prompt('Digite algo ou "sair" para encerrar o programa:');
 
-    if (entrada.toLowerCase() !== 'sair') {
-        console.log(`Você digitou ${entrada}`);
+    if (entrada === 'sair' || entrada === 'SAIR') {
+        break; 
     }
+
+    console.log(`Você digitou ${entrada}`);
 }
 
 console.log('Programa encerrado.');
